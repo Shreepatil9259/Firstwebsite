6d9f2edd75be32d98bd571f3288ff40f63862aad
Shreyas Patil <160312979+Shreepatil9259@users.noreply.github.com>
2025-03-29 21:08:24 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ export default function Contact() {
                         <form>
                             <div class="mb-3">
                                 <label for="exampleInputEmail1" class="form-label">Name</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                <input type="text" class="form-control" id="exampleInputPassword1" aria-describedby="emailHelp" />
                             </div>
                             <div class="mb-3">
                                 <label for="exampleInputPassword1" class="form-label">Surname</label>
